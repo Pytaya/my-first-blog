e51e90b622078084da96ca254d50f0ab432ef5b6
xiao <xiao@qq.com>
2017-11-29 19:48:40 +0800

    change setting's language to english

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,8 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'zh_Hans'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Shanghai'
